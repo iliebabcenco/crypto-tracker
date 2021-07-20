a9c202b413efff9d9bfe5e9998c8ce6d47c05fce
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-07-20 10:48:44 +0300

    Add tests fro reducers

--- src/tests/api/fetchData.test.js
+++ src/tests/api/fetchData.test.js
@@ -1,6 +1,6 @@
-import fetchCurrencies from '../../api/fetcher';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import fetchCurrencies from '../../api/fetcher';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -12,17 +12,20 @@ describe('setup store action', () => {
   });
 
   describe('when fetch data', () => {
-    it('should be correct actions for fetching currencies', () =>
-      store
-        .dispatch(fetchCurrencies(
-        ))
-        .then(() => expect(store.getActions()).toContainEqual({ "type": "FETCH_CURRENCY_BEGIN" }, { "currencies": [Array], "type": "FETCH_CURRENCY_SUCCESS" })));
-  })
-
-  it('gets the array of curriencies with a legth = 100', () =>
-    store
+    it('should include begin and succes actions for fetching currencies', () => store
       .dispatch(fetchCurrencies(
-        100
       ))
-      .then(() => expect((store.getActions())).toContainEqual({ "type": "FETCH_CURRENCY_BEGIN" }, { "currencies": [Array], "type": "FETCH_CURRENCY_SUCCESS" })));
-})
+      .then(() => expect(store.getActions()).toContainEqual({ type: 'FETCH_CURRENCY_BEGIN' }, { currencies: [Array], type: 'FETCH_CURRENCY_SUCCESS' })));
+  });
+
+  it('gets the array of curriencies with a legth = 100', () => store
+    .dispatch(fetchCurrencies(
+      100,
+    ))
+    .then(() => expect((store.getActions())).toContainEqual({ type: 'FETCH_CURRENCY_BEGIN' }, { currencies: [Array], type: 'FETCH_CURRENCY_SUCCESS' })));
+  // it('is a negative test getting error for bad params', () => store
+  //   .dispatch(fetchCurrencies(
+  //     'lala',
+  //   ))
+  //   .then(() => expect((store.getActions())).toContainEqual({ "type": "FETCH_CURRENCY_BEGIN" }, { "payload": { "error": [Error] }, "type": "FETCH_CURRENCY_FAILURE" })));
+});
